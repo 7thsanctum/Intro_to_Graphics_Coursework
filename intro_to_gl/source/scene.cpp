5bb34cdad4cba714422415086a06cd9244aa4440
7thsanctum <markmmiller@hotmail.co.uk>
2020-07-21 23:37:36 +0100

    Remove boost dependecy

--- intro_to_gl/source/scene.cpp
+++ intro_to_gl/source/scene.cpp
@@ -2,8 +2,8 @@
 #include "json.h"
 #include "util.h"
 
-#include <boost\property_tree\json_parser.hpp>
 #include <cstdlib>
+#include <iostream>
 #include <sstream>
 
 // TODO: I made a mess of this, clean it up.
@@ -82,24 +82,17 @@ T getNumberByName(json_object_element_s *root, std::string name) {
   return 0;
 }
 
-// Takes a branch of a property tree and reads in a vec3
-glm::vec3 readVec3(const boost::property_tree::ptree &pt) {
+glm::vec3 readVec3(json_array_s *array) {
   glm::vec3 v;
-  boost::property_tree::ptree::const_iterator iter = pt.begin();
-  v.x = (iter++)->second.get_value<float>();
-  v.y = (iter++)->second.get_value<float>();
-  v.z = (iter++)->second.get_value<float>();
-  return v;
-}
 
-// Takes a branch of a property tree and reads in a vec4
-glm::vec4 readVec4(const boost::property_tree::ptree &pt) {
-  glm::vec4 v;
-  boost::property_tree::ptree::const_iterator iter = pt.begin();
-  v.x = (iter++)->second.get_value<float>();
-  v.y = (iter++)->second.get_value<float>();
-  v.z = (iter++)->second.get_value<float>();
-  v.w = (iter++)->second.get_value<float>();
+  struct json_array_element_s *element_x = array->start;
+  struct json_array_element_s *element_y = element_x->next;
+  struct json_array_element_s *element_z = element_y->next;
+  struct json_array_element_s *element_w = element_z->next;
+
+  v.x = atof(json_value_as_number(element_x->value)->number);
+  v.y = atof(json_value_as_number(element_y->value)->number);
+  v.z = atof(json_value_as_number(element_z->value)->number);
   return v;
 }
 
@@ -134,8 +127,7 @@ void readGeometry(scene_data *scene, json_object_s *node) {
       std::string shape =
           ((json_string_s *)getValueByName(object->start, "shape")->payload)
               ->string;
-      if (std::is_same<int, int>::value) {
-      }
+
       geometry *geom;
 
       // TODO: Convert types to use integral consts rather than strings
@@ -162,7 +154,6 @@ void readGeometry(scene_data *scene, json_object_s *node) {
         int radius = getNumberByName<float>(object->start, "radius");
         int slices = getNumberByName<int>(object->start, "slices");
         int stacks = getNumberByName<int>(object->start, "stacks");
-        std::cout << "radius : " << radius << std::endl;
         geom = createTorus(radius, stacks, slices);
       } else if (shape == "plane") {
         int width = getNumberByName<int>(object->start, "width");
@@ -213,13 +204,11 @@ void readTextures(scene_data *scene, json_object_s *node) {
 }
 
 // Read in material data from the scene file
-void readMaterials(scene_data *scene, const boost::property_tree::ptree &pt,
-                   json_object_s *node) {
+void readMaterials(scene_data *scene, json_object_s *node) {
   json_object_element_s *a_node = node->start;
   do {
     struct json_string_s *a_name = a_node->name;
     std::string name = a_name->string;
-    // std::cout << "name : " << name << std::endl;
     struct json_object_s *object =
         (struct json_object_s *)a_node->value->payload;
     json_array_s *emissive = getArrayByName(object->start, "emmisive");
@@ -254,127 +243,131 @@ void readMaterials(scene_data *scene, const boost::property_tree::ptree &pt,
 
     a_node = a_node->next;
   } while (a_node != nullptr);
-
-  //   boost::property_tree::ptree::const_iterator iter = pt.begin();
-  //   // Iterate through all the sub branches, and read in the relevant data
-  //   for (; iter != pt.end(); ++iter) {
-  //     material *mat = new material();
-  //     std::string name = iter->first;
-  //     mat->data.emissive = readVec4(iter->second.get_child("emmisive"));
-  //     mat->data.ambient = readVec4(iter->second.get_child("ambient"));
-  //     mat->data.diffuse = readVec4(iter->second.get_child("diffuse"));
-  //     mat->data.specular = readVec4(iter->second.get_child("specular"));
-  //     mat->data.shininess =
-  //         iter->second.get_child("shininess").get_value<float>();
-  //     std::string texture =
-  //         iter->second.get_child("texture").get_value<std::string>();
-
-  //     // Try and find texture, and set material accordingly.  If the not
-  //     found,
-  //     // set to nullptr
-  //     if (scene->textures.find(texture) != scene->textures.end())
-  //       mat->texture = scene->textures[texture];
-  //     else
-  //       mat->texture = nullptr;
-
-  //     // Create the material, and add to the table
-  //     mat->create();
-  //     scene->material[name] = mat;
-  //   }
 }
 
 // Reads in a transform from the scene file
-void readTransform(render_object *object,
-                   const boost::property_tree::ptree &pt) {
-  object->transform.position = readVec3(pt.get_child("position"));
-  glm::vec3 rot = readVec3(pt.get_child("rotation"));
+void readTransform(render_object *object, json_object_element_s *transform) {
+  struct json_object_s *child =
+      (struct json_object_s *)transform->value->payload;
+  json_array_s *position = getArrayByName(child->start, "position");
+  json_array_s *rotation = getArrayByName(child->start, "rotation");
+  json_array_s *scale = getArrayByName(child->start, "scale");
+
+  object->transform.position = readVec3(position);
+  glm::vec3 rot = readVec3(rotation);
   object->transform.rotate(rot.x, glm::vec3(1.0f, 0.0f, 0.0f));
   object->transform.rotate(rot.y, glm::vec3(0.0f, 1.0f, 0.0f));
   object->transform.rotate(rot.z, glm::vec3(0.0f, 0.0f, 1.0f));
-  object->transform.scale = readVec3(pt.get_child("scale"));
+  object->transform.scale = readVec3(scale);
 }
 
 // Reads in all the render objects from the scene file
-void readObjects(scene_data *scene, const boost::property_tree::ptree &pt) {
-  boost::property_tree::ptree::const_iterator iter = pt.begin();
-
-  // Iterate through the sub branches of the property file, reading in each
-  // object
-  for (; iter != pt.end(); ++iter) {
+void readObjects(scene_data *scene, json_object_s *node) {
+  json_object_element_s *a_node = node->start;
+  do {
     // Create a render object
     render_object *object = new render_object();
 
     // Read in the name for the object
-    std::string name = iter->first;
+    std::string name = a_node->name->string;
+
+    struct json_object_s *child =
+        (struct json_object_s *)a_node->value->payload;
 
     // Read in the geometry
     std::string geom =
-        iter->second.get_child("geometry").get_value<std::string>();
+        ((json_string_s *)getValueByName(child->start, "geometry")->payload)
+            ->string;
     object->geometry = scene->geometry[geom];
 
     // Read in the material
     std::string mat =
-        iter->second.get_child("material").get_value<std::string>();
+        ((json_string_s *)getValueByName(child->start, "material")->payload)
+            ->string;
     object->material = scene->material[mat];
 
     // Read in the transform
-    readTransform(object, iter->second.get_child("transform"));
+    readTransform(object, getElementByName(child->start, "transform"));
 
     // Add the render object to the table of render objects
     scene->objects[name] = object;
-  }
+    a_node = a_node->next;
+  } while (a_node != nullptr);
 }
 
 // Reads in lighting data for a scene
-void readLighting(scene_data *scene, const boost::property_tree::ptree &pt) {
-  scene->light.data.ambient = readVec4(pt.get_child("ambient"));
-  scene->light.data.diffuse = readVec4(pt.get_child("diffuse"));
-  scene->light.data.specular = readVec4(pt.get_child("specular"));
-  scene->light.data.lightDir = readVec4(pt.get_child("lightDir"));
+void readLighting(scene_data *scene, json_object_s *node) {
+  json_array_s *ambient = getArrayByName(node->start, "ambient");
+  json_array_s *diffuse = getArrayByName(node->start, "diffuse");
+  json_array_s *specular = getArrayByName(node->start, "specular");
+  json_array_s *lightDir = getArrayByName(node->start, "lightDir");
+
+  scene->light.data.ambient = readVec4(ambient);
+  scene->light.data.diffuse = readVec4(diffuse);
+  scene->light.data.specular = readVec4(specular);
+  scene->light.data.lightDir = readVec4(lightDir);
   scene->light.create();
 }
 
 // Reads in data for a point light
-void readPointLight(scene_data *scene, const boost::property_tree::ptree &pt) {
+void readPointLight(scene_data *scene, json_object_s *node) {
+  json_array_s *ambient = getArrayByName(node->start, "ambient");
+  json_array_s *diffuse = getArrayByName(node->start, "diffuse");
+  json_array_s *specular = getArrayByName(node->start, "specular");
+  json_array_s *lightPos = getArrayByName(node->start, "lightPos");
+  json_array_s *attenuation = getArrayByName(node->start, "attenuation");
+
   point_light_data point;
-  point.ambient = readVec4(pt.get_child("ambient"));
-  point.diffuse = readVec4(pt.get_child("diffuse"));
-  point.specular = readVec4(pt.get_child("specular"));
-  point.lightPos = readVec4(pt.get_child("lightPos"));
-  point.attenuation = readVec4(pt.get_child("attenuation"));
-  point.dist = pt.get_child("dist").get_value<float>();
+  point.ambient = readVec4(ambient);
+  point.diffuse = readVec4(diffuse);
+  point.specular = readVec4(specular);
+  point.lightPos = readVec4(lightPos);
+  point.attenuation = readVec4(attenuation);
+  point.dist = getNumberByName<float>(node->start, "dist");
   scene->dynamic.addPoint(point);
 }
 
 // Reads in data for a spot light
-void readSpotLight(scene_data *scene, const boost::property_tree::ptree &pt) {
+void readSpotLight(scene_data *scene, json_object_s *node) {
+  json_array_s *ambient = getArrayByName(node->start, "ambient");
+  json_array_s *diffuse = getArrayByName(node->start, "diffuse");
+  json_array_s *specular = getArrayByName(node->start, "specular");
+  json_array_s *lightPos = getArrayByName(node->start, "lightPos");
+  json_array_s *attenuation = getArrayByName(node->start, "attenuation");
+  json_array_s *lightDir = getArrayByName(node->start, "lightDir");
+
   spot_light_data spot;
-  spot.ambient = readVec4(pt.get_child("ambient"));
-  spot.diffuse = readVec4(pt.get_child("diffuse"));
-  spot.specular = readVec4(pt.get_child("specular"));
-  spot.lightPos = readVec4(pt.get_child("lightPos"));
-  spot.attenuation = readVec4(pt.get_child("attenuation"));
-  spot.lightDir = readVec4(pt.get_child("lightDir"));
-  spot.power = pt.get_child("power").get_value<float>();
-  spot.dist = pt.get_child("dist").get_value<float>();
+  spot.ambient = readVec4(ambient);
+  spot.diffuse = readVec4(diffuse);
+  spot.specular = readVec4(specular);
+  spot.lightPos = readVec4(lightPos);
+  spot.attenuation = readVec4(attenuation);
+  spot.lightDir = readVec4(lightDir);
+  spot.power = getNumberByName<float>(node->start, "power");
+  spot.dist = getNumberByName<float>(node->start, "dist");
   scene->dynamic.addSpot(spot);
 }
 
 // Read in all the dynamic lights for the scene
-void readDynamicLights(scene_data *scene,
-                       const boost::property_tree::ptree &pt) {
-  boost::property_tree::ptree::const_iterator iter = pt.front().second.begin();
-  for (; iter != pt.front().second.end(); ++iter) {
-    if (iter->first == "point")
-      readPointLight(scene, iter->second);
-    else if (iter->first == "spot")
-      readSpotLight(scene, iter->second);
-    else {
-      std::cerr << "Error - unknown dynamic light encountered: "
-                << iter->second.front().first << std::endl;
+void readDynamicLights(scene_data *scene, json_array_s *array) {
+  struct json_array_element_s *element = array->start;
+  struct json_object_s *object = json_value_as_object(element->value);
+  json_object_element_s *a_node = object->start;
+
+  do {
+    // Read in the name for the object
+    std::string name = a_node->name->string;
+    if (name == "point") {
+      readPointLight(scene, (struct json_object_s *)a_node->value->payload);
+    } else if (name == "spot") {
+      readSpotLight(scene, (struct json_object_s *)a_node->value->payload);
+    } else {
+      std::cerr << "Error - unknown dynamic light encountered: " << name
+                << std::endl;
       exit(EXIT_FAILURE);
     }
-  }
+    a_node = a_node->next;
+  } while (a_node != nullptr);
   scene->dynamic.create();
 }
 
@@ -409,19 +402,17 @@ scene_data *loadScene(const std::string &fileName) {
   struct json_object_s *objects_node = getChildByName(object->start, "objects");
   struct json_object_s *lighting_node =
       getChildByName(object->start, "lighting");
-  struct json_object_s *dynamic_lighting_node =
-      getChildByName(object->start, "dynamic_lighting");
+  struct json_array_s *dynamic_lighting_node =
+      getArrayByName(object->start, "dynamic_lighting");
 
   scene_data *scene = new scene_data;
-  boost::property_tree::ptree pt;
-  boost::property_tree::read_json(fileName, pt);
 
   readTextures(scene, textures_node);
   readGeometry(scene, geometry_node);
-  readMaterials(scene, pt.get_child("materials"), materials_node);
-  readObjects(scene, pt.get_child("objects"));
-  readLighting(scene, pt.get_child("lighting"));
-  readDynamicLights(scene, pt.get_child("dynamic_lighting"));
+  readMaterials(scene, materials_node);
+  readObjects(scene, objects_node);
+  readLighting(scene, lighting_node);
+  readDynamicLights(scene, dynamic_lighting_node);
 
   free(root);
   return scene;
